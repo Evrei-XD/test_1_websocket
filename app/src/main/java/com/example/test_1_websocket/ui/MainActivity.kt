e6aac5e187d0548328a11f865a952cfbe0121570
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2022-05-08 16:23:49 +0300

    add viewModel to mainactivity

--- app/src/main/java/com/example/test_1_websocket/ui/MainActivity.kt
+++ app/src/main/java/com/example/test_1_websocket/ui/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.test_1_websocket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.test_1_websocket.R
+import com.example.test_1_websocket.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mViewModel: MainViewModel
+
+    private var connect = false
+    private var response = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        mViewModel.connect.observe(this) {
+            connect = it
+            if (it) {
+//                output_tv.text = ""
+//                start_btn.text = "connect"
+            } else {
+//                output_tv.text = ""
+//                start_btn.text = "disconnect"
+            }
+        }
+        mViewModel.mResponseSocket.observe(this) {
+            response = it
+//            output_tv.text = it
+        }
+
+//        start_btn.setOnClickListener {
+//            mViewModel.start()
+//        }
     }
 }
