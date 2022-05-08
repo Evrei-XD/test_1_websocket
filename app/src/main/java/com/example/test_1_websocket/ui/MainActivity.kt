2e016b137e88b0f9d2e678d2e6a172d94a70b704
Evrei-XD <30876844+Evrei-XD@users.noreply.github.com>
2022-05-08 16:16:44 +0300

    create xml

--- app/src/main/java/com/example/test_1_websocket/ui/MainActivity.kt
+++ app/src/main/java/com/example/test_1_websocket/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.test_1_websocket
+package com.example.test_1_websocket.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test_1_websocket.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
